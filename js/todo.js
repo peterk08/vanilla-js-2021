4514b43d904ace4961f1c6a613f51ca57688dc18
peterk08 <juna08@naver.com>
2021-10-03 22:19:47 +0900

    Update todo.js
    dfdfddfdf

--- js/todo.js
+++ js/todo.js
@@ -14,13 +14,14 @@ function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
     toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.Text;
 
     const button = document.createElement("button");
     button.innerText = `💥`;
@@ -34,14 +35,20 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo); 
-    paintToDo(newTodo); 
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj); 
+    paintToDo(newTodoObj); 
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-if (saveToDos !== null) {
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
     const parsedToDos = JSON.parse(saveToDos);
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
